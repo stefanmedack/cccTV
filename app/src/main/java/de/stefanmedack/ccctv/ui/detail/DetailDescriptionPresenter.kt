fb42fbc47a209601f5355c590badf41e27ec4b37
stefanmedack <stefan.medack@kitchenstories.de>
2017-09-07 23:13:41 +0200

    improves description displaying on detail page

--- app/src/main/java/de/stefanmedack/ccctv/ui/detail/DetailDescriptionPresenter.kt
+++ app/src/main/java/de/stefanmedack/ccctv/ui/detail/DetailDescriptionPresenter.kt
@@ -1,6 +1,9 @@
 package de.stefanmedack.ccctv.ui.detail
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.support.v17.leanback.widget.AbstractDetailsDescriptionPresenter
+import android.text.Html
 import de.stefanmedack.ccctv.model.MiniEvent
 
 class DetailDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
@@ -10,8 +13,21 @@ class DetailDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
             item: Any) {
         val event = item as MiniEvent
 
-        viewHolder.title.text = event.title
-        viewHolder.subtitle.text = event.subtitle
-        viewHolder.body.text = event.description
+        viewHolder.title.text = event.title.stripHtml()
+
+        val separator = if (event.subtitle.stripHtml().isNotEmpty()) "\n\n" else ""
+        @SuppressLint("SetTextI18n")
+        viewHolder.subtitle.text = "${event.subtitle.stripHtml()}$separator${event.description.stripHtml()}"
+        // TODO for some reason, leanback body can not display all content -> find solution and use next 2 lines again
+        //        viewHolder.subtitle.text = event.subtitle.stripHtml()
+        //        viewHolder.body.text = event.description.stripHtml()
     }
+
+    private fun String.stripHtml(): String =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                Html.fromHtml(this, Html.FROM_HTML_MODE_COMPACT).toString()
+            } else {
+                @Suppress("DEPRECATION")
+                Html.fromHtml(this).toString()
+            }
 }
